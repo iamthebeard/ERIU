33c87e3832241d4e5110b4f4ac4f77ad069f8c97
Tony <iamthebeard@gmail.com>
2026-07-21 08:49:44 -0600

    Episode 22: J&F pt3 - fix loading and 64bit error

--- Assets/Scripts/Character/Player/PlayerStatsManager.cs
+++ Assets/Scripts/Character/Player/PlayerStatsManager.cs
@@ -17,13 +17,16 @@ public class PlayerStatsManager : CharacterStatsManager
         base.Start();
 
         // When we have a class select menu, we'll set these there.
-        player.playerNetworkManager.maxHealth.Value =
-            CalculateMaxHealthBasedOnVitalityLevel(player.playerNetworkManager.vitality.Value);
-        PlayerUIManager.instance.playerUIHUDManager.SetMaxHealthValue(player.playerNetworkManager.maxHealth.Value);
-        player.playerNetworkManager.currentHealth.Value = player.playerNetworkManager.maxHealth.Value;
-        player.playerNetworkManager.maxStamina.Value =
-            CalculateMaxStaminaBasedOnEnduranceLevel(player.playerNetworkManager.endurance.Value);
-        PlayerUIManager.instance.playerUIHUDManager.SetMaxStaminaValue(player.playerNetworkManager.maxStamina.Value);
-        player.playerNetworkManager.currentStamina.Value = player.playerNetworkManager.currentStamina.Value;
+        if (player.IsOwner)
+        {
+            player.playerNetworkManager.maxHealth.Value =
+                CalculateMaxHealthBasedOnVitalityLevel(player.playerNetworkManager.vitality.Value);
+            PlayerUIManager.instance.playerUIHUDManager.SetMaxHealthValue(player.playerNetworkManager.maxHealth.Value);
+            player.playerNetworkManager.currentHealth.Value = player.playerNetworkManager.maxHealth.Value;
+            player.playerNetworkManager.maxStamina.Value =
+                CalculateMaxStaminaBasedOnEnduranceLevel(player.playerNetworkManager.endurance.Value);
+            PlayerUIManager.instance.playerUIHUDManager.SetMaxStaminaValue(player.playerNetworkManager.maxStamina.Value);
+            player.playerNetworkManager.currentStamina.Value = player.playerNetworkManager.currentStamina.Value;
+        }
     } 
 }
