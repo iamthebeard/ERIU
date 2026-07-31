073408cc233ee92b34671c3009ed11795aca219b
Tony <iamthebeard@gmail.com>
2026-07-31 09:00:25 -0600

    Ep 37: AI Character pt 4 -- smooth network motion

---
+++ Assets/Scripts/Character/AI Character/AICharacterAnimatorManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICharacterAnimatorManager : CharacterAnimatorManager
+{
+    AICharacterManager aiCharacter;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        aiCharacter = GetComponent<AICharacterManager>();
+    }
+
+    private void OnAnimatorMove()
+    {
+        if (aiCharacter.IsOwner)
+        {
+            if (!aiCharacter.isGrounded) return;
+
+            Vector3 velocity = aiCharacter.animator.deltaPosition;
+            aiCharacter.characterController.Move(velocity);
+            aiCharacter.transform.rotation *= aiCharacter.animator.deltaRotation;
+        }
+        else // Client
+        {
+            if (!aiCharacter.isGrounded) return;
+
+            Vector3 velocity = aiCharacter.animator.deltaPosition;
+            aiCharacter.characterController.Move(velocity);
+            aiCharacter.transform.position = 
+                Vector3.SmoothDamp(
+                    transform.position,
+                    aiCharacter.characterNetworkManager.networkPosition.Value,
+                    ref aiCharacter.aiCharacterNetworkManager.networkPositionVelocity,
+                    aiCharacter.aiCharacterNetworkManager.networkPositionSmoothTime
+                );
+            aiCharacter.transform.rotation *= aiCharacter.animator.deltaRotation;
+        }
+
+    }
+}
